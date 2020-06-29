14e2e52ca687690e7f9beca9d64298a169fac8ae
chrisvsanchez <chrisvsanchez@gmail.com>
2020-06-29 13:03:43 -0400

    I tried my best

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,25 @@
 class Review
+    attr_accessor :viewer, :movie, :rating
+    @@all = []
+        
+    def initialize(viewer, movie, rating)
+        @viewer = viewer
+        @movie = movie 
+        @rating = rating
+        @@all << self
+    end
 
+
+    def self.all 
+        @@all
+    end
+
+    def viewer
+    Viewer.all.find{|viewers_instance| viewers_instance == self.viewer }
+    end
+
+    def movie 
+        Movie.all.find{|movie_instance| movie_instance == self.movie}
+    end
 
 end
