d38c0ffb6d720211107accc9e5007dddc3c926e4
gracetian2019 <gracetian4ever@gmail.com>
2020-06-29 13:00:15 -0400

    not done yet

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,31 @@ class Viewer
   def self.all
     @@all
   end
-  
+
+  def reviews
+    #review instance related with viewer inst
+    #the one reviewwriter name == viewer username
+    Review.all.select{
+      |review| review.name == self
+    }
+  end
+  def reviewed_movies 
+    #reviewed*movie inst related with viewer
+    #map or collect both are ok
+    reviews.map{|review| review.movie}
+  end
+  def reviewed_movie?(movie)
+    #conditional
+    if reviewed_movies.movie == movie
+      true
+    else
+      false
+    end
+    def rate_movie(movie,rating)
+      #conditional
+      #if reviewed_movie? == true
+      #review.rating == rating
+      #else
+      #Review.new(self, movie, rating)
+    end
 end
