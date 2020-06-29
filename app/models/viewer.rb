c01fe9fcf0830868691e74a2a61b053eabf1296a
Fraciscv <Francisco.Andaur@icloud.com>
2020-06-29 12:09:05 -0400

    return array of review instance associated with the viewer instance

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -13,6 +13,9 @@ class Viewer
   end
 
   #returns an array of `Review` instances associated with the `Viewer` 
+  def reviews
+    Review.all.select {|r| r.viewer == self}
+  end
 
   #returns an array of `Movie` instances reviewed by the `Viewer` instance.
   
