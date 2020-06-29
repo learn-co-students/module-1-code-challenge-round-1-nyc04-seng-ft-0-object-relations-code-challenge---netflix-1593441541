e91ba9701f66f0bdc1edf5f936fee1acf797367e
21shield <21netaly@gmail.com>
2020-06-29 11:02:07 -0400

    new initialize method review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,17 @@
 class Review
-
+    @@all = []
+    attr_reader :viewer, :movie, :rating
+    def initialize(viewer, movie, rating)
+        @viewer = viewer
+        @movie = movie
+        @rating = rating
+        save
+    end
+    def self.all
+        @@all
+    end
+    def save
+        self.class.all << save
+    end
 
 end
