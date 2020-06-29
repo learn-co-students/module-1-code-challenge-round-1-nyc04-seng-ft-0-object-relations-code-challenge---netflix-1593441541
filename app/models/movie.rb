8523e9e9e6530c3991e47561d8a8d79c68d4b271
kceron <karemiceron@gmail.com>
2020-06-29 13:02:05 -0400

    what i did

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,36 @@ class Movie
     @@all
   end
 
+  #returns an array of all the Review instances for the Movie.
+  def reviews
+    Review.all.select {|rev| rev.movie_inst == self}
+  end
+  
+  #returns an array of all of the Viewer instances that reviewed the Movie.
+  def reviewers 
+    Review.all.select do |rev|
+      if rev.movie_inst == self
+        rev.viewer_inst
+      end
+    end
+  end
+
+  #helper method 
+  def helper_method
+    self.reviews.map {|rev| rev.rating_num}.count
+  end
+  #returns the average of all ratings for the Movie instance
+  def average_rating
+    self.reviews.map {|rev| rev.rating_num}.reduce(0){|sum, r| sum + r / helper_method}
+  end
+
+  #returns the Movie instance with the highest average rating.
+  def self.highest_rated
+    Review.all.map {|rev| rev.rating_num }
+
+  end
+  #array.max_by do |element|
+  #element.field
+end
+
 end
