124619577a09a03d79e3945bd9130263304d3fa3
kelseyjdickson <kelseymedved@gmail.com>
2020-06-29 12:36:59 -0400

    methods review and reviewers

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,12 @@
+###- `Movie#reviews`
+  ##- returns an array of all the `Review` instances for the `Movie`. 
+  
+  ###- `Movie#reviewers`
+   # - returns an array of all of the `Viewer` instances that reviewed the `Movie`.
+
+
+
+
 class Movie
   attr_accessor :title
 
@@ -6,6 +15,19 @@ class Movie
   def initialize(title)
     @title = title
     self.class.all << self
+  end
+
+  def reviews
+   Review.all.select do |movie_instance|
+    movie_instance.movie
+   end
+  end
+  
+  def reviewers
+    Viewer.all.select do |viewer_instance|
+      
+      viewer_instance.username
+    end
   end
 
   def self.all
