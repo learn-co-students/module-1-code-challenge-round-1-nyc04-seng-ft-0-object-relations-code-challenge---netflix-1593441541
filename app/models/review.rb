083baf333d96cf8831638d4f52e3d53466ff6d27
kelseyjdickson <kelseymedved@gmail.com>
2020-06-29 11:56:13 -0400

    ratings method

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,25 @@
 class Review
 
+  
+  
+    
+    @@all = []
+    attr_reader :viewer, :movie, :rating
+    def initialize(viewer, movie, rating)
+        @viewer = viewer
+        @movie = movie
+        @rating = rating 
+        @@all << self
+    end
+
+    def rating
+        Review.all.map do |review_instance|
+            review_instance.rating
+        end
+    end
+
+    def self.all
+        @@all
+    end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -10,7 +10,25 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+#MOVIE (title)
 
+m1 = Movie.new("Bad Boys")
+m2 = Movie.new("Legally Blonde")
+m3 = Movie.new("Romeo & Juliet")
+
+#VIEWER (username)
+
+u1 = Viewer.new("cool_dude")
+u2 = Viewer.new("dad_movies")
+u3 = Viewer.new("mamama123")
+
+# REVIEW (viewer, movie, rating)
+
+r1 = Review.new(u1, m1, 7)
+r2 = Review.new(u2, m2, 7)
+r3 = Review.new(u3, m3, 7)
+
+p r1
 
 
 
