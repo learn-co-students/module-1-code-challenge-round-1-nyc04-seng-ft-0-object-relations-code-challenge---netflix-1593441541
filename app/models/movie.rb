34fbf2f740ade8c425b234b9cb14d337ff5fce19
guillenjs <jorgeguilln@gmail.com>
2020-06-29 12:31:34 -0400

    built movie reviews

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,13 @@ class Movie
     @@all
   end
 
+#   - `Movie#reviews`
+#   - returns an array of all the `Review` instances for the `Movie`.
+def reviews
+  Review.all.select {|review| review.movie == self }
+end
+
+# - `Movie#reviewers`
+#   - returns an array of all of the `Viewer` instances that reviewed the `Movie`.
+
 end
